ac3ff1ca8ad72f8a4bab242ad4c6b832d4ff81d7
milkowska <weronikamilka1706@gmail.com>
2022-11-26 16:52:57 +0000

    adding menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool paused; 
+    public GameObject MidMenuUI;
+
+     void Start()
+    {
+        paused = false;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(paused)
+            {
+                Resume();
+            }else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Pause()
+    {
+        MidMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+    }
+
+    public void Resume()
+    {
+        MidMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("FirstRoom");
+    }
+}
